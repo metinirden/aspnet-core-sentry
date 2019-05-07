c25599118718f6c9d3a87d5470bfb8003bec5156
metinirden <ExtMetinI@d-teknoloji.com.tr>
2019-05-07 13:20:17 +0300

    project created. :tada:

---
+++ UI/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(ILogger<HomeController> logger) => Logger = logger;
+
+        public ILogger<HomeController> Logger { get; }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                var divider = 0;
+                var result = 5 / divider;
+            }
+            catch (System.Exception ex)
+            {
+                Logger.LogCritical(ex, ex.Message);
+            }
+
+            return Json(new { message = "helloo !" });
+        }
+    }
+}
